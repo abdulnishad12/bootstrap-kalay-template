0a795c85ad032c732458a29e8817f43aefd0bc6f
costaleonardo <costaleonardo@gmail.com>
2018-03-28 23:53:36 -0400

    Add files.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp'),
+      gulpCleanCss = require('gulp-clean-css'),
+      gulpSass = require('gulp-sass'),
+      gulpMinifyJs = require('gulp-js-minify'),
+      gulpRename = requre('gulp-rename');
+
+// Compile scss files
+gulp.task('compile-scss', () => {
+  return gulp.src('./scss/style.scss')
+             .pipe(gulpSass({ errLogToConsole: true }))
+             .pipe(gulp.dest('css'));
+});
+
+// Watch for scss changes
+gulp.task('watch-scss', () => {
+  gulp.watch('./scss/**/*scss', ['compile-scss']);
+});
+
+// Minify css files
+gulp.task('minify-css', () => {
+  return gulp.src('./css/style.css')
+             .pipe(gulpCleanCss({ compatibility: 'ie8' }))
+             .pipe(gulpRename('style.min.css'))
+             .pipe(gulp.dest('./css'));
+});
+
+// Minify js files
+gulp.task('minify-js', () => {
+  return gulp.src('./js/main.js')
+             .pipe(gulpMinifyJs())
+             .pipe(gulpRename('main.min.js'))
+             .pipe(gulp.dest('./js'));
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+<a align="center" href=""><img width="100%" src="img/website.png" alt="Text"></a>
+
+>  Template description.
+
+## Getting Started
+
+Clone or download ZIP and open index.html.
+
+## License
+
+Coded by [Leonardo Da Costa](https://github.com/costaleonardo). Licensed under the [MIT License](https://cjpatoilo.mit-license.org).
